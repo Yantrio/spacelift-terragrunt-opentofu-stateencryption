59cbb6bcbcfd6079e409c58d633b6a9505685ad5
James Humphries <james@james-humphries.co.uk>
2024-04-25 16:32:26 +0100

    Added stack dependency
    Signed-off-by: James Humphries <james@james-humphries.co.uk>

--- main.tf
+++ main.tf
@@ -5,13 +5,6 @@ terraform {
       version = "1.12.0"
     }
   }
-
-  backend "s3" {
-    bucket = "terragrunt-james-testing-bucket"
-    key    = "spacelift/terragrunt/main.tfstate"
-    region = "eu-west-1"
-  }
-
 }
 
 provider "spacelift" {
@@ -42,9 +35,45 @@ resource "spacelift_stack" "terragrunt-stack" {
   runner_image = "ghcr.io/opentofu/opentofu:1.7.0-rc1"
 }
 
+resource "spacelift_stack" "terragrunt-stack2" {
+  terragrunt {
+    terragrunt_version     = "0.55.15"
+    use_run_all            = false
+    use_smart_sanitization = true
+    tool                   = "OPEN_TOFU"
+  }
+
+  autodeploy   = true
+  branch       = "main"
+  name         = "Terragrunt stack example 2 - opentofu and state encryption"
+  description  = "Deploys infra using Terragrunt"
+  repository   = "spacelift-terragrunt-opentofu-stateencryption"
+  project_root = "terragrunt2"
+
+  runner_image = "ghcr.io/opentofu/opentofu:1.7.0-rc1"
+}
+
 resource "spacelift_aws_integration_attachment" "my_stack" {
   integration_id = "01HWAWFHSK0FYRCSYZ71AK8V5Y"
   stack_id       = spacelift_stack.terragrunt-stack.id
   read           = true
   write          = true
+}
+
+resource "spacelift_aws_integration_attachment" "my_stack" {
+  integration_id = "01HWAWFHSK0FYRCSYZ71AK8V5Y"
+  stack_id       = spacelift_stack.terragrunt-stack.id
+  read           = true
+  write          = true
+}
+
+resource "spacelift_stack_dependency" "dep" {
+  stack_id            = spacelift_stack.terragrunt-stack2.id
+  depends_on_stack_id = spacelift_stack.terragrunt-stack.id
+}
+
+resource "spacelift_stack_dependency_reference" "dep" {
+  stack_dependency_id = spacelift_stack_dependency.dep.id
+  output_name         = "terragrunt/id"
+  input_name          = "TF_VAR_id"
 }
