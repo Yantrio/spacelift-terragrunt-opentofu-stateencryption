1f98429b75d27213ae3eabfae0b4205b766803f2
James Humphries <james@james-humphries.co.uk>
2024-04-25 15:49:10 +0100

    more
    Signed-off-by: James Humphries <james@james-humphries.co.uk>

--- terragrunt/main.tf
+++ terragrunt/main.tf
@@ -13,6 +13,15 @@ resource "random_password" "password" {
   override_special = "_%@"
 }
 
+resource "random_id" "id" {
+  byte_length = 4
+}
+
 output "password" {
-  value = random_password.password.result
+  value     = random_password.password.result
+  sensitive = true
+}
+
+output "id" {
+  value = random_id.id.hex
 }
